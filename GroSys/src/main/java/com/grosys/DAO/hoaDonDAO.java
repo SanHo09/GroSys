0d5ef14a679ecd6d275ccb692e9c067ee0ebd894
thanhftrung <trungntps18841@fpt.edu.vn>
2021-11-11 10:34:56 +0700

    chi tiet

---
+++ GroSys/src/main/java/com/grosys/DAO/hoaDonDAO.java
@@ -0,0 +1,83 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.grosys.DAO;
+
+import com.grosys.help.Xjdbc;
+import com.grosys.untity.hoaDon;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author HP
+ */
+public class hoaDonDAO extends grosysDAO<hoaDon, String>{
+
+    
+    public void insert(hoaDon model) {
+       String sql ="INSERT INTO HoaDon(MaHD, NgayLHD, SoTien, MaNV)VALUES(?,?,?,?)";
+       Xjdbc.update(sql,
+               model.getMaHD(),
+               model.getNgayLHD(),
+               model.getSoTien(),
+               model.getMaNV());
+    }
+
+    
+    public void update(hoaDon model) {
+        String sql ="UPDATE HoaDon SET NgayLHD=?, SoTien=?, MaNV=? Where MaHD=?";
+        Xjdbc.update(sql,
+               model.getNgayLHD(),
+               model.getSoTien(),
+               model.getMaNV());
+    }
+
+    
+    public void delete(String MaHD) {
+        String sql="DELETE FROM HoaDon WHERE MaHD=?";
+        Xjdbc.update(sql, MaHD);
+    }
+
+    
+    public List<hoaDon> selectAll() {
+        String sql="SELECT*FROM HoaDon";
+        return this.selectBySql(sql);
+    }
+
+    
+    public hoaDon selectById(String mahd) {
+        String sql="SELECT * FROM HoaDon WHERE MaHD=?";
+        List<hoaDon> list = selectBySql(sql, mahd);
+        return list.size() > 0 ? list.get(0) : null;
+    }
+
+    
+    protected List<hoaDon> selectBySql(String sql, Object... args) {
+        List<hoaDon> list = new ArrayList<hoaDon>();
+        try {
+            ResultSet rs = null;
+            try {
+                rs = Xjdbc.query(sql, args);
+                while(rs.next()){
+                    hoaDon entity =new hoaDon();
+                    entity.setMaHD(rs.getString("MaHD"));
+                    entity.setNgayLHD(rs.getString("NgayLHD"));
+                    entity.setSoTien(rs.getInt("SoTien"));
+                    entity.setMaNV(rs.getString("MaNV"));
+                    list.add(entity);
+                }
+            } finally{
+                rs.getStatement().getConnection().close();
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            throw new RuntimeException(ex);
+        }
+        return list;
+    }
+    
+}
---
+++ GroSys/src/main/java/com/grosys/untity/hoaDon.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.grosys.untity;
+
+/**
+ *
+ * @author HP
+ */
+public class hoaDon {
+    private String maHD;
+    private String ngayLHD;
+    private double soTien;
+    private String maNV;
+
+    public String getMaHD() {
+        return maHD;
+    }
+
+    public void setMaHD(String maHD) {
+        this.maHD = maHD;
+    }
+
+    public String getNgayLHD() {
+        return ngayLHD;
+    }
+
+    public void setNgayLHD(String ngayLHD) {
+        this.ngayLHD = ngayLHD;
+    }
+
+    public double getSoTien() {
+        return soTien;
+    }
+
+    public void setSoTien(double soTien) {
+        this.soTien = soTien;
+    }
+
+    public String getMaNV() {
+        return maNV;
+    }
+
+    public void setMaNV(String maNV) {
+        this.maNV = maNV;
+    }
+    
+    
+}
