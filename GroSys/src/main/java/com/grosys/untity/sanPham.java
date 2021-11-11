2f084724af751b88dc499ac631e830791fb645da
Tarashaki <tarashaki4s@gmail.com>
2021-11-11 08:06:01 +0700

    Du AN 1

---
+++ GroSys/src/main/java/com/grosys/untity/sanPham.java
@@ -0,0 +1,88 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.grosys.untity;
+
+import java.util.Date;
+
+/**
+ *
+ * @author HP
+ */
+public class sanPham {
+    private String maSP;
+    private String tenSP;
+    private String maLSP;
+    private double giaBan;
+    private Date hanSuDung;
+    private String donViTinh;
+    private int soLuong;
+    private String anh;
+
+    public String getMaSP() {
+        return maSP;
+    }
+
+    public void setMaSP(String maSP) {
+        this.maSP = maSP;
+    }
+
+    public String getTenSP() {
+        return tenSP;
+    }
+
+    public void setTenSP(String tenSP) {
+        this.tenSP = tenSP;
+    }
+
+    public String getMaLSP() {
+        return maLSP;
+    }
+
+    public void setMaLSP(String maLSP) {
+        this.maLSP = maLSP;
+    }
+
+    public double getGiaBan() {
+        return giaBan;
+    }
+
+    public void setGiaBan(double giaBan) {
+        this.giaBan = giaBan;
+    }
+
+    public Date getHanSuDung() {
+        return hanSuDung;
+    }
+
+    public void setHanSuDung(Date hanSuDung) {
+        this.hanSuDung = hanSuDung;
+    }
+
+    public String getDonViTinh() {
+        return donViTinh;
+    }
+
+    public void setDonViTinh(String donViTinh) {
+        this.donViTinh = donViTinh;
+    }
+
+    public int getSoLuong() {
+        return soLuong;
+    }
+
+    public void setSoLuong(int soLuong) {
+        this.soLuong = soLuong;
+    }
+
+    public String getAnh() {
+        return anh;
+    }
+
+    public void setAnh(String anh) {
+        this.anh = anh;
+    }
+    
+}
